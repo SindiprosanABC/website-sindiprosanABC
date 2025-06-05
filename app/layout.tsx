685971d661fed9d7b75c57269cd9cbfe457edebd
Sindiprosan ABC <sandro.oliveira1@yahoo.com.br>
2025-06-05 18:43:51 -0300

    fix: remove chat-widget component

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <ChatWidget />
+        {/* <ChatWidget /> */}
         <TopBar />
         <Navbar />
         {children}
