dd554e6687d08670dbe2928a79a417ffb3c85398
Sindiprosan ABC <sandro.oliveira1@yahoo.com.br>
2025-06-05 19:02:09 -0300

    feat: edit chat-widget component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { TopBar } from "@/components/top-bar";
 import { Navbar } from "@/components/sections/navbar";
+import { ChatWidget } from "@/components/chat-widget";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <TopBar />
         <Navbar />
         {children}
+        <ChatWidget />
       </body>
     </html>
   );
