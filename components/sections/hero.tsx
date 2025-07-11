6dc64650bad8e83c16ec85f092d20da84b4a5518
SindiprosanABC <sandro.oliveira1@yahoo.com.br>
2025-07-11 10:59:27 -0300

    feat: edit button text hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -43,7 +43,7 @@ export const Hero = () => {
                 variant="outline"
                 className="border-white text-white hover:bg-white hover:text-[#2e4b89]"
               >
-                Associa-se
+                Associe-se
               </Button>
             </Link>
           </div>
